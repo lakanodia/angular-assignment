8ee8b7120230650cea535e9b6596a68ba6ed9181
Laka Nodia <laka.nodia@gmail.com>
2022-11-13 16:26:14 +0400

    Display infromationa about users

--- src/app/features/components/user-list/user-list.component.ts
+++ src/app/features/components/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IUser } from '../../models/user.interface';
 
 @Component({
   selector: 'app-user-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
+  @Input() users: IUser[] = [];
+
+
   constructor() {}
 
   ngOnInit(): void {}
