08c51255c331c752e72a1843707a8e61c6870f00
Laka Nodia <laka.nodia@gmail.com>
2022-11-13 16:15:58 +0400

    Add users data in user components

--- src/app/features/components/users.component.ts
+++ src/app/features/components/users.component.ts
@@ -7,7 +7,34 @@ import { IUser } from '../models/user.interface';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-  
+  usersData: IUser[] = [
+    {
+      id: 1,
+      age: 23,
+      email: 'email@gmail,com',
+      mobile: '588779966',
+      name: 'John',
+      lastName: 'Doe',
+    },
+
+    {
+      id: 2,
+      age: 21,
+      email: 'email@gmail,com',
+      mobile: '577889966',
+      name: 'Kate',
+      lastName: 'Meyer',
+    },
+
+    {
+      id: 3,
+      age: 29,
+      email: 'email@gmail,com',
+      mobile: '548779966',
+      name: 'Anna',
+      lastName: 'Lopes',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
