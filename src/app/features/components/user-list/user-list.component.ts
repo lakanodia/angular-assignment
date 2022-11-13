01db876383221fbcc771c34ee2c74fb454ed081c
Laka Nodia <laka.nodia@gmail.com>
2022-11-13 16:30:14 +0400

    Add selectuser Event

--- src/app/features/components/user-list/user-list.component.ts
+++ src/app/features/components/user-list/user-list.component.ts
@@ -8,9 +8,14 @@ import { IUser } from '../../models/user.interface';
 })
 export class UserListComponent implements OnInit {
   @Input() users: IUser[] = [];
-
+  @Output() selectUserEvent = new EventEmitter<number>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  selectUser(id: number) {
+    this.selectUserEvent.emit(id);
+    console.log(id);
+  }
 }
