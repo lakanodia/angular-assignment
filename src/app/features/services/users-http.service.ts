7a60080a7807f1795e58446bd4caedeef7c16c94
Laka Nodia <laka.nodia@gmail.com>
2022-11-17 21:35:06 +0400

    Add http get ,ethod

---
+++ src/app/features/services/users-http.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IUser } from '../models/user.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersHttpService {
+  backendUrl = 'http://localhost:3000/users';
+  constructor(private httpClient: HttpClient) {}
+
+  getUsers() {
+    return this.httpClient.get<IUser[]>(this.backendUrl);
+  }
+}
